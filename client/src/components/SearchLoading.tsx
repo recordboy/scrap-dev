5fe1b0b5afd06678144ccea88263329ac00078f1
recordboy <ksj_design@naver.com>
2021-05-02 21:05:53 +0900

    로딩바 추가중

--- client/src/components/SearchLoading.tsx
+++ client/src/components/SearchLoading.tsx
@@ -3,7 +3,9 @@ import React from "react";
 const SearchLoading = (props: { isOnLoading: boolean }) => {
   const { isOnLoading } = props;
   return (
-    <div className={isOnLoading ? "loading on" : "loading"}>loading..</div>
+    <div className={isOnLoading ? "loading on" : "loading"}><div className="loading-inner">
+      <div></div><div></div><div></div><div></div><div></div>
+    </div></div>
   );
 };
 
